4f8b5a4b059fb70f4eb441cc0a8cb6b41ca54459
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-08 06:10:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(num)
     current_card_total = card_total + deal_card
     return current_card_total
   else 
-    puts "Please enter a valid command."
+    invalid_command
     prompt_user
 end
 
